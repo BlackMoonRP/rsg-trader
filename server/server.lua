1fabddb89d43b5f5308ed0fdaf790303959dc481
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 15:11:08 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,36 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-trader/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 -- get market data
 RSGCore.Functions.CreateCallback('rsg-trader:server:getmarketdata', function(source, cb)
     local marketdata = MySQL.query.await('SELECT * FROM market_data', {})
@@ -79,3 +110,8 @@ TradingInterval = function()
 end
 
 SetTimeout(Config.TraderCycle * (60 * 1000), TradingInterval) -- mins
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
